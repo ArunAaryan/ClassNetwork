a61c48a2e209c534786ebc9b2e8cad59de9b905a
Arun Aaryan <rnyadav98@gmail.com>
2020-01-17 23:07:46 +0530

    added delete and make teacher functionality in students tab of admin

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,13 @@ const express = require("express");
 const passport = require("passport");
 const Posts = require("../models/Posts");
 const Admin = require("../models/Admin");
-const Users = require("../models/User");
+const User = require("../models/User");
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require("../config/auth");
 
 router.get("/viewteachers", ensureAuthenticated, (req, res) => {
   if (req.user.currentuser === "admin") {
-    Users.find(
+    User.find(
       {
         currentuser: "teacher"
       },
@@ -21,11 +21,11 @@ router.get("/viewteachers", ensureAuthenticated, (req, res) => {
           console.log(err);
           return;
         }
-        console.log(typeof teacher);
-        console.log(teacher);
-        console.log(teacher.length);
-        console.log(teacher[0]);
-       res.render('viewteachers',{teacher:teacher})
+        // console.log(typeof teacher);
+        // console.log(teacher);
+        // console.log(teacher.length);
+        // console.log(teacher[0]);
+        res.render("viewteachers", { teacher: teacher });
       }
     ); //student users
   } else {
@@ -33,38 +33,124 @@ router.get("/viewteachers", ensureAuthenticated, (req, res) => {
   } //check for current user
 }); //close the request(admin-getuserlist)
 
-router.post('/viewteachers',(req,res)=>{
-  res.send(req.body)
-})
-
-
-
-
+router.post("/viewteachers", (req, res) => {
+  //this is to delete teachers
+  console.log(req.body)
+  if (req.user.currentuser === "admin") {
+    let userid = req.body.userid;
+    if(!userid){
+      req.flash('error_msg','No users selected!')
+      res.redirect('/admin/viewteachers')
+      return;
+    }
+    if (typeof(userid) === "string") {
+      
+        userid = [userid]
+      }
+      // console.log(posts[0])
+      // console.log(posts.length)
+      
+      for(var i = 0;i<userid.length;i++){
+        // console.log(userid[i])
+        // console.log(typeof(userid[i]))
+        User.deleteOne({_id: userid[i]}).then(user=>{
+      }).catch(err=>console.log(err));
+           
+    }
+      req.flash('success_msg', 'Deleted ' +userid.length+'users');
+      res.redirect('/admin/viewteachers')
+           
+    }//if
+   else {
+    res.send("!unauthorized");
+  }
+  // res.send(req.body);
+  // console.log(req.body.userid);
+  // console.log(typeof req.body.userid);
+});
 
 router.get("/viewstudents", ensureAuthenticated, (req, res) => {
   if (req.user.currentuser === "admin") {
-    Users.find(
+    User.find(
       {
         currentuser: "student"
+      },
+      {
+        _id: 1,
+        name: 1
       },
       (err, student) => {
         if (err) {
           console.log(err);
+          return;
         }
+       
+        res.render("viewstudents", { student: student });
       }
-      
-
-    );
-    // console.log(teacher)
-    // console.log(typeof(teacherlist))
-    res.render('viewstudents',student);
-    // res.render('viewusers',{
-    //   teacherlist,
-    //   studentlist
-    // })//res.render close
+    ); //student users
   } else {
     res.send("!unauthorized");
-  } //check for current user
-}); //close the request(admin-getuserlist)
+  } //check for cur
+});
 
+
+router.post("/viewstudents", (req, res) => {
+  //this is to delete teachers
+  console.log(req.body)
+  if (req.user.currentuser === "admin") {
+    let userid = req.body.userid;
+    if(!req.body.toggleAction){
+      req.flash('error','Select action type')
+      res.redirect('/admin/viewstudents')
+      return
+    }
+    if(!userid){
+      req.flash('error_msg','No users selected!')
+      res.redirect('/admin/viewstudents')
+      return;
+    }
+    if(req.body.toggleAction==="setTeacher"){
+      if (typeof(userid) === "string") {
+      
+        userid = [userid]
+      }
+      // console.log(posts[0])
+      // console.log(posts.length)
+      
+      for(var i = 0;i<userid.length;i++){
+        // console.log(userid[i])
+        // console.log(typeof(userid[i]))
+        User.updateOne({_id: userid[i]},{$set:{currentuser:"teacher"}}).then(user=>{
+      }).catch(err=>console.log(err));
+           
+    }
+      req.flash('success_msg', 'Updated' +userid.length+'users');
+      res.redirect('/admin/viewstudents')
+    }
+    else{
+    if (typeof(userid) === "string") {
+      
+        userid = [userid]
+      }
+      // console.log(posts[0])
+      // console.log(posts.length)
+      
+      for(var i = 0;i<userid.length;i++){
+        // console.log(userid[i])
+        // console.log(typeof(userid[i]))
+        User.deleteOne({_id: userid[i]}).then(user=>{
+      }).catch(err=>console.log(err));
+           
+    }
+      req.flash('success_msg', 'Deleted ' +userid.length+' users');
+      res.redirect('/admin/viewstudents')
+  }//else
+    }//if admin
+   else {
+    res.send("!unauthorized");
+  }
+  // res.send(req.body);
+  // console.log(req.body.userid);
+  // console.log(typeof req.body.userid);
+});
 module.exports = router;
