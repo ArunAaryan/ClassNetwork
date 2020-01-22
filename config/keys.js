4722e84c08354f05a666b877768c9cae5f369bca
Arun Aaryan <rnyadav98@gmail.com>
2020-01-22 13:16:42 +0530

    second commit

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,10 @@
 
 // 'mongodb+srv://<username>:<password>@<clustername>.mongodb.net/<collectionname>?retryWrites=true&w=majority';
 if(process.env.NODE_ENV==="production"){
+    console.log('in production')
     module.exports = require("./prod")
 }else{
+    console.log('not in production')
     module.exports = require("./devkey")
 }
 
